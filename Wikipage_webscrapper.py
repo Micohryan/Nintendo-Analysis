a3a94510937ac6974b027bf2a099a4bb9b7deb3b
Micohryan <micohryan@gmail.com>
2023-02-22 16:39:34 -0800

    Added comments to program

--- Wikipage_webscrapper.py
+++ Wikipage_webscrapper.py
@@ -3,17 +3,20 @@ import requests
 from datetime import date
 import pandas as pd
 
+#Url of webpage
 wikiurl= "https://en.wikipedia.org/wiki/List_of_best-selling_Nintendo_Switch_video_games"
-table_class="wikitable sortable jquery-tablesorter"
 response=requests.get(wikiurl)
 
 soup = BeautifulSoup(response.text, 'html.parser')
-table=soup.find('table',{'class':"wikitable"})
-table
 
+#finds html tag table with class attribute wikitable
+table=soup.find('table',{'class':"wikitable"})
+
+#convert the variable table string into list
 df=pd.read_html(str(table))
+
 # convert list to dataframe
 df=pd.DataFrame(df[0])
 print(df.head())
-
+# convert to CSV
 df.to_csv('nintendo.csv')
